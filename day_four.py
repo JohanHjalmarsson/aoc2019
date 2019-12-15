f10bfd4effd2214853f0edc46966145e57d3a76d
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2019-12-15 22:03:24 +0100

    day four a

--- day_four.py
+++ day_four.py
@@ -1,14 +1,16 @@
-low = 172930
+low = 172930 
 high = 683082
 amount = 0
 while low < high:
     prev = -1
+    add = False
     for x in str(low):
         if int(x) == prev:
-            amount = amount + 1
+            add = True
         if int(x) < prev:
-            amount = amount - 1
+            add = False
+            break
         prev = int(x)
+    amount = add and amount + 1 or amount
     low = low + 1
-
 print(amount)
